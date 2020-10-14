7990b05c0eccccd706dc987b7ae2c330c99b0d11
Ivan Alles <ivan.alles@gmail.com>
2020-10-14 16:42:12 +0200

    Check orthogonality of a conv2d kernel.

--- simple_orthogonality_test.py
+++ simple_orthogonality_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 # For a conv2d  I -> O tensorflow keeps the kernel in tensors of shape H x W x I x O.
 
@@ -42,3 +43,42 @@ print(p1)
 
 p2 = np.tensordot(a, a, axes=[[0], [0]])
 print(p2)
+
+H = 2
+W = 3
+INPUTS = 4
+OUTPUTS = 5
+
+kernel = np.ones((H, W, INPUTS, OUTPUTS), dtype=np.float32)
+
+# Make 0th and 1st kernels orthogonal
+
+kernel[:, :, :, 0] = np.array([
+    [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 7, 8, 6],
+    ],
+    [
+        [5, 4, 3, 2],
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+    ]
+])
+
+s = np.sum(kernel[:, :, :, 0]) - 1
+
+kernel[0, 0, 0, 1] = -s
+
+conv = tf.keras.layers.Conv2D(
+    OUTPUTS,
+    (H, W),
+    kernel_initializer=tf.keras.initializers.Constant(kernel),
+)
+
+model = tf.keras.Sequential([tf.keras.layers.Input(shape=(100, 100, INPUTS)), conv])
+print(conv.kernel.shape)
+print(conv.kernel.numpy())
+
+d = tf.tensordot(conv.kernel, conv.kernel, [[0, 1, 2], [0, 1, 2]])
+print(d)
